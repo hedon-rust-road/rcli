00e252283865281369d3a504029d390beafc8936
hedon954 <171725713@qq.com>
2024-04-25 00:25:32 +0800

    feature: csv to json

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,62 @@
-// rcli csv -i input.csv -o output.json --header -d ','
-
 use std::fs;
 
-use anyhow::Ok;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 use crate::player::Player;
 
 mod player;
 
 #[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
 struct Opts {
-    #[arg(short, long)]
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+/// rcli csv -i input.csv -o output.json --header -d ','
+#[derive(Debug, Subcommand)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    /// Input file path
+    #[arg(short, long, value_parser = verify_input_file)]
     input: String,
 
+    /// Output file path
     #[arg(short, long, default_value = "output.json")]
     output: String,
 
+    /// CSV has header or not
     #[arg(long, default_value_t = false)]
     header: bool,
 
+    /// Delimiter
     #[arg(short, long, default_value_t = ',')]
-    deperator: char,
+    delimiter: char,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if std::path::Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File dose not exists")
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let mut reader = csv::Reader::from_path(opts.input)?;
 
-    let f = fs::File::open(opts.input)?;
-    let mut reader = csv::Reader::from_reader(f);
-
-    for record in reader.deserialize() {
-        let player: Player = record?;
-        println!("{:?}", player);
+            let players: Vec<Player> = reader.deserialize::<Player>().map(|r| r.unwrap()).collect();
+            let res = serde_json::to_string_pretty(&players)?;
+            fs::write(opts.output, res)?;
+        }
     }
-
     Ok(())
 }
--- src/player.rs
+++ src/player.rs
@@ -1,15 +1,13 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Player {
     pub name: String,
     pub position: String,
-
     #[serde(rename = "DOB")]
     pub dob: String,
     pub nationality: String,
-
     #[serde(rename = "Kit Number")]
     pub kit_number: String,
 }
