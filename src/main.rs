93eefc5ebb5081779eea9ec124eb8b7a8f9c8fdb
hedon954 <171725713@qq.com>
2024-04-25 22:36:32 +0800

    refactor: csv

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,10 @@
-use std::fs;
-
-use clap::{Parser, Subcommand};
-use rcli::player::Player;
-
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-/// rcli csv -i input.csv -o output.json --header -d ','
-#[derive(Debug, Subcommand)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    /// Input file path
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-
-    /// Output file path
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-
-    /// CSV has header or not
-    #[arg(long, default_value_t = false)]
-    header: bool,
-
-    /// Delimiter
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-}
-
-fn verify_input_file(filename: &str) -> Result<String, &'static str> {
-    if std::path::Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File dose not exists")
-    }
-}
+use clap::Parser;
+use rcli::{opts, process::process_csv};
 
 fn main() -> anyhow::Result<()> {
-    let opts = Opts::parse();
+    let opts = opts::Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let mut reader = csv::Reader::from_path(opts.input)?;
-
-            let players: Vec<Player> = reader.deserialize::<Player>().map(|r| r.unwrap()).collect();
-            let res = serde_json::to_string_pretty(&players)?;
-            fs::write(opts.output, res)?;
-        }
+        opts::SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
     }
     Ok(())
 }
